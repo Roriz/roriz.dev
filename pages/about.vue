35f26905384641845f591d03658d10c8740a0276
Radamés Roriz <radamesroriz@gmail.com>
2024-12-13 13:52:26 -0300

    ref: use SSG instead of SPA

--- pages/about.vue
+++ pages/about.vue
@@ -15,7 +15,7 @@
     <p>I’m a software engineer with a passion for building innovative solutions.</p>
     <p>With over 15 years of experience in software development, I’ve contributed to a wide range of projects, spanning from agile startups to large-scale enterprise applications.  </p>
     <p>My enthusiasm for technology stems from its ability to solve real-world challenges. This passion has driven my entrepreneurial journey, starting with founding my first company, <b>Aibol</b> (2014), followed by launching my own consulting firm (2015). From 2016 to 2018, I collaborated with a startup builder company, helping launch <b>Docker</b>, <b>Vestgo</b>, <b>Obba</b>, <b>Vinixo</b>, and <b>Sova360</b>. In 2018, I joined the founding team of <b>Dr.Entrega</b> and later became a partner at <b>Revelo</b> in 2020.</p>
-    <p>I thrive on generating and executing new ideas—thinking through every aspect from conception to execution: the business model, the technology, the team, the market, and the clients.</p>
+    <p>I thrive on generating and executing new ideas, thinking through every aspect from conception to execution: the business model, the technology, the team, the market, and the clients.</p>
     <p>Currently, I’m captivated by the potential of generative AI and its transformative impact on the future, often imagining innovations yet to come.</p>
     <p>This space is a platform for me to share my thoughts, projects, achievements, and anything else I find inspiring.</p>
     <p>If any of this resonates with you, feel free to connect with me on <a href="https://www.linkedin.com/in/radames-roriz/">LinkedIn</a> or reach out via <a href="mailto:radamesroriz@gmail.com">email</a>.</p>
@@ -23,9 +23,6 @@
 </template>
 
 <style scoped>
-.about {
-}
-
 .profile {
   display: flex;
   flex-direction: column;
@@ -62,7 +59,6 @@
 }
 
 p {
-  /* improve reading for long term */
   line-height: 1.5;
   margin-bottom: 1rem;
   font-size: 1.2rem;
